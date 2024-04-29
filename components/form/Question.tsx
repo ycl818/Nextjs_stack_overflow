5987320e1182b46a323785661af5704a005d3959
yichien <kevinyichienlee@gmail.com>
2024-04-29 17:09:25 +0800

    add tinymce-react editor

--- components/form/Question.tsx
+++ components/form/Question.tsx
@@ -1,4 +1,6 @@
 "use client";
+import { useRef } from "react";
+import { Editor } from "@tinymce/tinymce-react";
 
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
@@ -15,8 +17,12 @@ import {
 } from "../ui/form";
 import { Input } from "../ui/input";
 import { QuestionsSchema } from "@/lib/validation";
+import { ThemeContextType, useTheme } from "@/context/ThemeProvider";
 
 const Question = () => {
+  const { mode } = useTheme() as ThemeContextType;
+  const editorRef = useRef(null);
+
   // 1. Define your form.
   const form = useForm<z.infer<typeof QuestionsSchema>>({
     resolver: zodResolver(QuestionsSchema),
@@ -72,7 +78,44 @@ const Question = () => {
                 Detailed explaination of your problem
                 <span className="text-primary-500">*</span>
               </FormLabel>
-              <FormControl className="mt-3.5">{/* Add editor */}</FormControl>
+              <FormControl className="mt-3.5">
+                <Editor
+                  apiKey={process.env.NEXT_PUBLIC_TINY_EDITOR_API_KEY}
+                  onInit={(_evt, editor) => {
+                    // @ts-ignore
+                    editorRef.current = editor;
+                  }}
+                  initialValue=""
+                  init={{
+                    height: 350,
+                    menubar: false,
+                    plugins: [
+                      "advlist",
+                      "autolink",
+                      "lists",
+                      "link",
+                      "image",
+                      "charmap",
+                      "preview",
+                      "anchor",
+                      "searchreplace",
+                      "visualblocks",
+                      "codesample",
+                      "fullscreen",
+                      "insertdatetime",
+                      "media",
+                      "table",
+                    ],
+                    toolbar:
+                      "undo redo | " +
+                      "codesample | bold italic forecolor | alignleft aligncenter |" +
+                      "alignright alignjustify | bullist numlist",
+                    content_style: "body { font-family:Inter; font-size:16px }",
+                    skin: mode === "dark" ? "oxide-dark" : "oxide",
+                    content_css: mode === "dark" ? "dark" : "light",
+                  }}
+                />
+              </FormControl>
               <FormDescription className="body-regular mt-2.5 text-light-500">
                 Introduce the problem and expand on what you put in the title.
                 Minimum 10 characters
