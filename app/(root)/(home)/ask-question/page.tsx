0150b61f8692baf01dd6ab486ebd49b4fa6bbc49
yichien <kevinyichienlee@gmail.com>
2024-05-03 23:50:10 +0800

    update questionCard avatar, and UPDATE nectconfig for allowing images from outside

--- app/(root)/(home)/ask-question/page.tsx
+++ app/(root)/(home)/ask-question/page.tsx
@@ -1,13 +1,11 @@
 import Question from "@/components/form/Question";
 import { getUserById } from "@/lib/actions/user.action";
-// import { auth } from "@clerk/nextjs/server";
+import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 import React from "react";
 
 const Page = async () => {
-  // const { userId } = auth();
-
-  const userId = "123456789";
+  const { userId } = auth();
 
   if (!userId) {
     redirect("/sign-in");
--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // experimental: {
-  //   serverComponentsExternalPackages: ["mongoose"],
-  // },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "*",
+      },
+      {
+        protocol: "http",
+        hostname: "*",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
