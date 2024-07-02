9db8c5f6def3920fcc5afd4f6891b28b055835c6
yichien <kevinyichienlee@gmail.com>
2024-07-02 23:30:37 +0800

    display all saved questions

--- lib/actions/shared.types.d.ts
+++ lib/actions/shared.types.d.ts
@@ -88,3 +88,11 @@ export interface ToggleSaveQuestionParams {
   questionId: string;
   path: string;
 }
+
+export interface GetSavedQuestionsParams {
+  clerkId: string;
+  page?: number;
+  pageSize?: number;
+  filter?: string;
+  searchQuery?: string;
+}
--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -5,11 +5,15 @@ import { connectToDatabase } from "../mongoose";
 import {
   CreateUserParams,
   GetAllUsersParams,
+  GetSavedQuestionsParams,
   ToggleSaveQuestionParams,
   UpdateUserParams,
 } from "./shared.types";
 import { revalidatePath } from "next/cache";
 import Question from "@/database/question.model";
+import Tag from "@/database/tag.model";
+import { FilterQuery } from "mongoose";
+import { create } from "domain";
 
 export async function getUserById(params: any) {
   try {
@@ -132,6 +136,41 @@ export async function toggleSaveQuestion(params: ToggleSaveQuestionParams) {
     }
 
     revalidatePath(path);
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+
+export async function getSavedQuestions(params: GetSavedQuestionsParams) {
+  try {
+    connectToDatabase();
+
+    const { clerkId, searchQuery, filter, page = 1, pageSize = 10 } = params;
+
+    const query: FilterQuery<typeof Question> = searchQuery
+      ? { title: { $regex: new RegExp(searchQuery, "i") } }
+      : {};
+
+    const user = await User.findOne({ clerkId }).populate({
+      path: "saved",
+      match: query,
+      options: {
+        sort: { createdAt: -1 },
+      },
+      populate: [
+        { path: "tags", model: Tag, select: "_id name" },
+        { path: "author", model: User, select: "_id clerkId name picture" },
+      ],
+    });
+
+    if (!user) {
+      throw new Error("User not found");
+    }
+
+    const savedQuestions = user.saved;
+
+    return { questions: savedQuestions };
   } catch (error) {
     console.log(error);
     throw error;
